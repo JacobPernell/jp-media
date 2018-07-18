bf3925874ed7f9e36219c2b2399d48498a5130d7
Jacob Pernell <jacob.pernell@gmail.com>
2018-07-17 20:48:30 -0700

    Added contact section and started working on the portfolio overlay

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const portfolioOverlay = document.querySelector('.portfolio__overlay');
+const portfolioOverlayTitle = document.querySelector('.portfolio__overlay--title');
 
 function togglePortfolioOverlay() {
     portfolioOverlay.classList.toggle("open");
 }
+
+const clipboard = new ClipboardJS('.contact__info');
+
+clipboard.on('success', function(e) {
+    document.querySelector('.contact__info--sub').innerHTML = "Email copied!";
+    e.clearSelection();
+});
