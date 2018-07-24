739681c368e97d60d8840a2793e8d7027ae6d3b2
Jacob Pernell <jacob.pernell@gmail.com>
2018-07-24 01:42:28 -0700

    Reworked the services section.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,81 @@
-const portfolioOverlay = document.querySelector('.portfolio__overlay');
-const portfolioOverlayTitle = document.querySelector('.portfolio__overlay--title');
-const navBar = document.querySelector('.header');
+// const portfolioOverlay = document.querySelector('.portfolio__overlay');
+// const portfolioOverlayTitle = document.querySelector('.portfolio__overlay--title');
+// function togglePortfolioOverlay(e) {
+//     portfolioOverlay.classList.toggle("open");
+//
+//     let portfolioItem = e.target.parentElement.parentElement.dataset.portfolioinfo;
+//     console.log(e.target.parentElement.parentElement.dataset.portfolioinfo);
+// }
+//
+// function closeOverlay() {
+//     portfolioOverlay.classList.toggle("open");
+// }
 
-function togglePortfolioOverlay(e) {
-    portfolioOverlay.classList.toggle("open");
+// Toggle what's displayed in the services section on hover
+const servicesTitle = document.querySelector('.services__info--title');
+const servicesVideo = document.querySelector('.services__info--video');
+const servicesText = document.querySelector('.services__info--text');
+function setServicesDisplay(e) {
+    let serviceType = e.target.dataset.servicetype;
 
-    let portfolioItem = e.target.parentElement.parentElement.dataset.portfolioinfo;
-    console.log(e.target.parentElement.parentElement.dataset.portfolioinfo);
+    switch (serviceType) {
+        case 'web':
+            servicesTitle.innerHTML = "Web Design";
+            servicesVideo.innerHTML = `
+                <video width="600" controls>
+                <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+                </video>`;
+                servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            break;
+        case 'ux':
+            servicesTitle.innerHTML = "UI / UX Design";
+            servicesVideo.innerHTML = `
+                <video width="600" controls>
+                <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+                </video>`;
+                servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            break;
+        case 'audio':
+            servicesTitle.innerHTML = "Sonic Branding";
+            servicesVideo.innerHTML = `
+                <video width="600" controls>
+                <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+                </video>`;
+                servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            break;
+        case 'video':
+            servicesTitle.innerHTML = "Video Production";
+            servicesVideo.innerHTML = `
+                <video width="600" controls>
+                <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+                </video>`;
+                servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            break;
+        case 'marketing':
+            servicesTitle.innerHTML = "Digital Content Marketing";
+            servicesVideo.innerHTML = `
+                <video width="600" controls>
+                <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+                </video>`;
+                servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            break;
+    }
 }
 
-function closeOverlay() {
-    portfolioOverlay.classList.toggle("open");
-}
-
+// Click to copy email to clipboard
 const clipboard = new ClipboardJS('.contact__info');
-
 clipboard.on('success', function(e) {
     document.querySelector('.contact__info--sub').innerHTML = "Email copied!";
     e.clearSelection();
 });
 
+// Navbar background fade on scroll
+const navBar = document.querySelector('.header');
 window.onscroll = function () {
     if (window.pageYOffset > 75 ) {
         navBar.classList.add("nav-colored");
