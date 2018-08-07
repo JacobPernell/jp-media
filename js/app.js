7285d2c794a2e343d2d5f366682f49ca19466c68
Jacob Pernell <jacob.pernell@gmail.com>
2018-08-06 23:28:04 -0700

    Fixed fading in Services section

--- js/app.js
+++ js/app.js
@@ -1,70 +1,69 @@
-// const portfolioOverlay = document.querySelector('.portfolio__overlay');
-// const portfolioOverlayTitle = document.querySelector('.portfolio__overlay--title');
-// function togglePortfolioOverlay(e) {
-//     portfolioOverlay.classList.toggle("open");
-//
-//     let portfolioItem = e.target.parentElement.parentElement.dataset.portfolioinfo;
-//     console.log(e.target.parentElement.parentElement.dataset.portfolioinfo);
-// }
-//
-// function closeOverlay() {
-//     portfolioOverlay.classList.toggle("open");
-// }
-
 // Toggle what's displayed in the services section on hover
 const servicesTitle = document.querySelector('.services__info--title');
 const servicesVideo = document.querySelector('.services__info--video');
 const servicesText = document.querySelector('.services__info--text');
+
 function setServicesDisplay(e) {
     let serviceType = e.target.dataset.servicetype;
+    let fadeSpeed = 110;
 
-    switch (serviceType) {
-        case 'web':
-            servicesTitle.innerHTML = "Web Design";
-            servicesVideo.innerHTML = `
+    $(".services__info--text").fadeOut(fadeSpeed);
+    $(".services__info--video").fadeOut(fadeSpeed);
+    $(".services__info--title").fadeOut(fadeSpeed);
+
+    setTimeout(function() {
+
+        switch (serviceType) {
+            case 'web':
+                servicesTitle.innerHTML = "Web Design";
+                servicesVideo.innerHTML = `
                 <video width="600" controls>
                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
                 </video>`;
                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            break;
-        case 'ux':
-            servicesTitle.innerHTML = "UI / UX Design";
-            servicesVideo.innerHTML = `
+                break;
+            case 'ux':
+                servicesTitle.innerHTML = "UI / UX Design";
+                servicesVideo.innerHTML = `
                 <video width="600" controls>
                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
                 </video>`;
                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            break;
-        case 'audio':
-            servicesTitle.innerHTML = "Sonic Branding";
-            servicesVideo.innerHTML = `
+                break;
+            case 'audio':
+                servicesTitle.innerHTML = "Sonic Branding";
+                servicesVideo.innerHTML = `
                 <video width="600" controls>
                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
                 </video>`;
                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            break;
-        case 'video':
-            servicesTitle.innerHTML = "Video Production";
-            servicesVideo.innerHTML = `
+                break;
+            case 'video':
+                servicesTitle.innerHTML = "Video Production";
+                servicesVideo.innerHTML = `
                 <video width="600" controls>
                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
                 </video>`;
                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            break;
-        case 'marketing':
-            servicesTitle.innerHTML = "Digital Content Marketing";
-            servicesVideo.innerHTML = `
+                break;
+            case 'marketing':
+                servicesTitle.innerHTML = "Digital Content Marketing";
+                servicesVideo.innerHTML = `
                 <video width="600" controls>
                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
                 </video>`;
                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            break;
-    }
+                break;
+        }
+    }, fadeSpeed);
+    $(".services__info--text").fadeIn(fadeSpeed);
+    $(".services__info--video").fadeIn(fadeSpeed);
+    $(".services__info--title").fadeIn(fadeSpeed);
 }
 
 // Click to copy email to clipboard
@@ -76,12 +75,11 @@ clipboard.on('success', function(e) {
 
 // Navbar background fade on scroll
 const navBar = document.querySelector('.header');
-window.onscroll = function () {
-    if (window.pageYOffset > 75 ) {
+window.onscroll = function() {
+    if (window.pageYOffset > 75) {
         navBar.classList.add("nav-colored");
         navBar.classList.remove("nav-transparent");
-    }
-    else {
+    } else {
         navBar.classList.add("nav-transparent");
         navBar.classList.remove("nav-colored");
     }
@@ -90,36 +88,43 @@ window.onscroll = function () {
 // Navbar click to scroll
 $(".top-nav__link--about").click(function() {
     $('html,body').animate({
-        scrollTop: $(".about").offset().top-$('.header').outerHeight()+20},
+            scrollTop: $(".about").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".top-nav__link--services").click(function() {
     $('html,body').animate({
-        scrollTop: $(".services").offset().top-$('.header').outerHeight()+20},
+            scrollTop: $(".services").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".top-nav__link--portfolio").click(function() {
     $('html,body').animate({
-        scrollTop: $(".portfolio").offset().top-$('.header').outerHeight()+20},
+            scrollTop: $(".portfolio").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".hero__learn-more").click(function() {
     $('html,body').animate({
-        scrollTop: $(".portfolio").offset().top-$('.header').outerHeight()+20},
+            scrollTop: $(".portfolio").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".top-nav__link--testimonials").click(function() {
     $('html,body').animate({
-        scrollTop: $(".testimonials").offset().top-$('.header').outerHeight()+20},
+            scrollTop: $(".testimonials").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".top-nav__link--contact").click(function() {
     $('html,body').animate({
-        scrollTop: $(".contact").offset().top-$('.header').outerHeight()},
+            scrollTop: $(".contact").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
 $(".hero__cta").click(function() {
     $('html,body').animate({
-        scrollTop: $(".contact").offset().top-$('.header').outerHeight()},
+            scrollTop: $(".contact").offset().top - $('.header').outerHeight() + 20
+        },
         'slow');
 });
