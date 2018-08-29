1b2795f5ae9187951d4a7930cede3fbdbe2eb96b
Jacob Pernell <jacob.pernell@gmail.com>
2018-08-28 18:02:12 -0700

    Started the portfolio overlay wipe

--- js/app.js
+++ js/app.js
@@ -3,69 +3,26 @@ const servicesTitle = document.querySelector('.services__info--title');
 const servicesVideo = document.querySelector('.services__info--video');
 const servicesText = document.querySelector('.services__info--text');
 const portfolioOverlay = document.querySelector('.overlay');
+const portfolioOverlayClose = document.querySelector('.overlay_close');
+let portOpen = false;
 
-// function setServicesDisplay(e) {
-//     let serviceType = e.target.dataset.servicetype;
-//     let fadeSpeed = 110;
-//
-//     $(".services__info--text").fadeOut(fadeSpeed);
-//     $(".services__info--video").fadeOut(fadeSpeed);
-//     $(".services__info--title").fadeOut(fadeSpeed);
-//
-//     setTimeout(function() {
-//
-//         switch (serviceType) {
-//             case 'web':
-//                 servicesTitle.innerHTML = "Web Design";
-//                 servicesVideo.innerHTML = `
-//                 <video width="600" controls>
-//                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
-//                 Your browser does not support HTML5 video.
-//                 </video>`;
-//                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//                 break;
-//             case 'ux':
-//                 servicesTitle.innerHTML = "UI / UX Design";
-//                 servicesVideo.innerHTML = `
-//                 <video width="600" controls>
-//                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
-//                 Your browser does not support HTML5 video.
-//                 </video>`;
-//                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//                 break;
-//             case 'audio':
-//                 servicesTitle.innerHTML = "Sonic Branding";
-//                 servicesVideo.innerHTML = `
-//                 <video width="600" controls>
-//                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
-//                 Your browser does not support HTML5 video.
-//                 </video>`;
-//                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//                 break;
-//             case 'video':
-//                 servicesTitle.innerHTML = "Video Production";
-//                 servicesVideo.innerHTML = `
-//                 <video width="600" controls>
-//                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
-//                 Your browser does not support HTML5 video.
-//                 </video>`;
-//                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//                 break;
-//             case 'marketing':
-//                 servicesTitle.innerHTML = "Digital Content Marketing";
-//                 servicesVideo.innerHTML = `
-//                 <video width="600" controls>
-//                 <source src="../img/services/jpm_musicreel.mp4" type="video/mp4">
-//                 Your browser does not support HTML5 video.
-//                 </video>`;
-//                 servicesText.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//                 break;
-//         }
-//     }, fadeSpeed);
-//     $(".services__info--text").fadeIn(fadeSpeed);
-//     $(".services__info--video").fadeIn(fadeSpeed);
-//     $(".services__info--title").fadeIn(fadeSpeed);
-// }
+// Show portfolio overlay
+function showPortfolioOverlay() {
+    portfolioOverlay.classList.toggle("overlayfullwidth");
+    if (!portOpen) {
+        portfolioOverlayClose.classList.add("portfoliodisplay");
+        portfolioOverlayClose.classList.remove("portfoliodisplaynone");
+        portOpen = true;
+    } else {
+        portfolioOverlayClose.classList.remove("portfoliodisplay");
+        portfolioOverlayClose.classList.add("portfoliodisplaynone");
+        portOpen = false;
+    }
+
+}
+
+// let portName = document.querySelector(`.overlay_${e.dataset.portfolio}`);
+// portName.classList.toggle("portfoliodisplay");
 
 // Click to copy email to clipboard
 const clipboard = new ClipboardJS('.contact__info');
